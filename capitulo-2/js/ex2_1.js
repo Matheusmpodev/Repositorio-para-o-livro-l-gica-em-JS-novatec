fb502e20a932aed0ef4b87f48d1e66dff1b98830
Matheusmpodev <Matheusmpo.dev@gmail.com>
2026-03-28 22:35:08 -0300

    fim ex1.1cap2

---
+++ capitulo-2/js/ex2_1.js
@@ -0,0 +1,10 @@
+let frm = document.querySelector("form"); //cria referencia ao form e ao h3
+let resp = document.querySelector("h3");
+
+//cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+resp.innerText = ""
+frm.addEventListener("submit", (e) => {
+    const nome = frm.inome.value //obtém o nome(proprieda) digitado no form
+    resp.innerText = `Olá ${nome}` //exibe a resposta do programa
+    e.preventDefault() //evita envio do form
+})
