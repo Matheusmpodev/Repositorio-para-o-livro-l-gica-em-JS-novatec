065d826e0254fde6e542b07621e1a71a10c61cff
Matheusmpodev <Matheusmpo.dev@gmail.com>
2026-03-28 22:41:08 -0300

    corrigindo organização

--- capitulo-2/js/ex2_1.js
+++ capitulo-2/js/ex2_1.js
@@ -2,7 +2,7 @@ let frm = document.querySelector("form"); //cria referencia ao form e ao h3
 let resp = document.querySelector("h3");
 
 //cria um "ouvinte" de evento, acionado quando o botão submit for clicado
-resp.innerText = ""
+resp.innerText = " "
 frm.addEventListener("submit", (e) => {
     const nome = frm.inome.value //obtém o nome(proprieda) digitado no form
     resp.innerText = `Olá ${nome}` //exibe a resposta do programa
