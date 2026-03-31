f999e3e68958cbf48a901e9d00e25519813c7c48
Matheusmpodev <Matheusmpo.dev@gmail.com>
2026-03-31 14:48:49 -0300

    salvei

---
+++ capitulo-3/ex3_1.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")()
+const num1 = number(prompt("1° número"))
+const num2 = number(prompt("2° numero:"))
+const soma = num1 + num2
+console.log(`Soma é: ${soma}`)
